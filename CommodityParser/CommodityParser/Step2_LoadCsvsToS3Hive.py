ebd9a9b3a1927fc4b59d40716cbbf743dcf2f086
mgsassu <44040709+mgsassu@users.noreply.github.com>
2024-02-29 19:04:21 -0700

    Moved CommoditiesDict to a new class

--- CommodityParser/CommodityParser/Step2_LoadCsvsToS3Hive.py
+++ CommodityParser/CommodityParser/Step2_LoadCsvsToS3Hive.py
@@ -16,41 +16,8 @@ def main():
     dt_now_str = str(t4)
     
     # Setup AWS clients and locations
-    # athena_client = boto3.client('athena')
-    # database = 'commdatabase'
-    
     s3_client = boto3.client('s3')
     s3_bucket = 's3bucket-comm'
-    
-    # # Commodities dict
-    # commodities_dict = {"Cotton": ["COTTON NO. 2 - ICE FUTURES U.S.", "COTTON NO. 2 - NEW YORK BOARD OF TRADE"],
-    #                     "WheatSRW": ["WHEAT-SRW - CHICAGO BOARD OF TRADE", "WHEAT - CHICAGO BOARD OF TRADE"],
-    #                     "WheatHRW": ["WHEAT-HRW - CHICAGO BOARD OF TRADE", "WHEAT - KANSAS CITY BOARD OF TRADE"],
-    #                     "WheatHRSpring": ["WHEAT-HRSpring - MINNEAPOLIS GRAIN EXCHANGE", "WHEAT - MINNEAPOLIS GRAIN EXCHANGE"],
-    #                     "Corn": ["CORN - CHICAGO BOARD OF TRADE"],
-    #                     "Soybeans": ["SOYBEANS - CHICAGO BOARD OF TRADE"],
-    #                     "SoybeanOil": ["SOYBEAN OIL - CHICAGO BOARD OF TRADE"],
-    #                     "SoybeanMeal": ["SOYBEAN MEAL - CHICAGO BOARD OF TRADE"],
-    #                     "RoughRice": ["ROUGH RICE - CHICAGO BOARD OF TRADE"],
-    #                     "FrznOJ": ["FRZN CONCENTRATED ORANGE JUICE - ICE FUTURES U.S."],
-    #                     "Butter": ["BUTTER (CASH SETTLED) - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "MilkClassIII": ["MILK, Class III - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "NonFatDryMilk": ["NON FAT DRY MILK - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "CMEMilkIV": ["CME MILK IV - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "Cheese": ["CHEESE (CASH-SETTLED) - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "LeanHogs": ["LEAN HOGS - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "LiveCattle": ["LIVE CATTLE - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "FeederCattle": ["FEEDER CATTLE - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "Cocoa": ["COCOA - ICE FUTURES U.S.", "COCOA - NEW YORK BOARD OF TRADE"],
-    #                     "Sugar": ["SUGAR NO. 11 - ICE FUTURES U.S.", "SUGAR NO. 11 - NEW YORK BOARD OF TRADE"],
-    #                     "Coffee": ["COFFEE C - ICE FUTURES U.S.", "COFFEE C - NEW YORK BOARD OF TRADE"],
-    #                     "PalmOil": ["USD Malaysian Crude Palm Oil C - CHICAGO MERCANTILE EXCHANGE"],
-    #                     "Canola": ["CANOLA - ICE FUTURES U.S."]}
-    
-    # # Dict of pandas dataframes, one for each of the keys in the above dict
-    # dataframe_dict = {}
-    # for key in commodities_dict.keys():
-    #     dataframe_dict.append()
     
     # Get all files
     path = "./OutputFiles_Reduced/*.csv"
